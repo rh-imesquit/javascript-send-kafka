6c86535d335f69136179a4ff0c94d783789d0b7e
rh-imesquit <ianbmesquita.dev@gmail.com>
2024-07-10 16:23:34 -0300

    feat: Creating an application for sending Kafka messages.

--- src/config/kafka.js
+++ src/config/kafka.js
@@ -3,14 +3,20 @@ require('dotenv').config();
 const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
-  clientId: 'my-app',
+  clientId: 'app-producer',
   brokers: [process.env.KAFKA_BROKER]
 });
 
 const producer = kafka.producer();
 
 const connectProducer = async () => {
-  await producer.connect();
+  try {
+    await producer.connect();
+    console.log('Conectado ao Kafka com sucesso!');
+  } catch (err) {
+    console.error(err.message);
+    throw new Error('Não foi possível conectar ao Kafka. Verifique a configuração e tente novamente.');
+  }
 };
 
 module.exports = {
